d9fe98cb852083e1aec71de8d317b2d7150dd042
Julie Provost <juliemasonprovost@gmail.com>
2023-01-05 08:20:10 -0600

    Lotsof changes

--- watched.js
+++ watched.js
@@ -17,3 +17,13 @@ const comingSoonBtn = document.getElementById("coming-soon-btn");
 comingSoonBtn.addEventListener("click", () => {
   window.location = "./comingsoon.html";
 });
+
+const container = document.querySelector('#container');
+const baseURL = 'https://soldierswifecrazylife.com/wp-content/uploads/2023/01/'
+
+for (let i = 1; i < 10; i++) {
+  const newImg = document.createElement('img');
+  newImg.scr = `${baseURL}${i}.jpg`
+  container.appendChild(newImg)
+}
+
