e4c92c9aae6a0872f027557dddbaeac3f6388274
Julie Provost <juliemasonprovost@gmail.com>
2023-01-06 07:34:27 -0600

    Changes

--- watching.js
+++ watching.js
@@ -1,8 +1,9 @@
 const container = document.querySelector('#container');
 const baseURL = 'https://soldierswifecrazylife.com/wp-content/uploads/2023/01/'
 
+
 for (let i = 1; i < 10; i++) {
     const newImg = document.createElement('img');
-    newImg.scr = `${baseURL}${i}.jpg`
+    newImg.src = `${baseURL}${i}.jpg`
     container.appendChild(newImg)
 }
