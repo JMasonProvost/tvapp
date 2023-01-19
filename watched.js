fba87ec634357a8d6f151daabafc26ad915cf1fc
Julie Provost <juliemasonprovost@gmail.com>
2023-01-19 10:02:50 -0600

    Add List Functions

--- watched.js
+++ watched.js
@@ -3,20 +3,33 @@
 //   alert("clicked");
 // });
 
-const watchingBtn = document.getElementById("watching-btn");
-watchingBtn.addEventListener("click", () => {
-  window.location = "./watching.html";
+const form = document.querySelector("#showsForm");
+const input = document.querySelector("#showsName");
+const list = document.querySelector("#list");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const showsName = input.value;
+  const newLI = document.createElement("LI");
+  newLI.innerText = showsName;
+  list.append(newLI);
+  input.value = "";
+
 });
 
-const watchedBtn = document.getElementById("watched-btn");
-watchedBtn.addEventListener("click", () => {
-  window.location = "./watched.html";
-});
+//const watchingBtn = document.getElementById("watching-btn");
+//watchingBtn.addEventListener("click", () => {
+// window.location = "./watching.html";
+//});
 
-const comingSoonBtn = document.getElementById("coming-soon-btn");
-comingSoonBtn.addEventListener("click", () => {
-  window.location = "./comingsoon.html";
-});
+//const watchedBtn = document.getElementById("watched-btn");
+//watchedBtn.addEventListener("click", () => {
+//  window.location = "./watched.html";
+//});
+
+//const comingSoonBtn = document.getElementById("coming-soon-btn");
+//comingSoonBtn.addEventListener("click", () => {
+// window.location = "./comingsoon.html";
+//});
 
 const container = document.querySelector('#container');
 const baseURL = 'https://soldierswifecrazylife.com/wp-content/uploads/2023/01/'
@@ -26,4 +39,6 @@ for (let i = 1; i < 10; i++) {
   newImg.scr = `${baseURL}${i}.jpg`
   container.appendChild(newImg)
 }
+
+
 
